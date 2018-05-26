a2e155b95873bad8f38b46853de328d05523fd94
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2018-05-26 01:07:41 -0500

    Enemy Functions
    Los enemigo ya tienen su seudo comportamiento

---
+++ Enlightment/Assets/_root/Enemies/Torrets/Torret_C.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torret_C : MonoBehaviour {
+
+	RaycastHit hit;
+	public Animator anim;
+
+	// Update is called once per frame
+	void Update () {
+		if (Physics.Raycast (transform.position, transform.TransformDirection (Vector3.right), out hit, Mathf.Infinity) && hit.transform.CompareTag ("Player")) {
+			Debug.DrawRay (transform.position, transform.TransformDirection (Vector3.right) * hit.distance, Color.yellow);
+			Debug.Log ("Estoy tocando : " + hit.transform.name);
+			anim.SetTrigger ("Detec");
+		} else {
+			anim.SetTrigger ("Desactive");
+		}
+	}
+}
