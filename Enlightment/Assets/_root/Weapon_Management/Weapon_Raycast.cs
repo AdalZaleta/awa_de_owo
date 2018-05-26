d25f53626e3f0c9d2d0d048338ddaf0128f5b70f
Retus001 <retus001@gmail.com>
2018-05-25 22:55:56 -0500

    Folder Organization
    + FPS Controller

---
+++ Enlightment/Assets/_root/Weapon_Management/Weapon_Raycast.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon_Raycast : Weapon_Main {
+
+	// Class Raycaster
+
+	[Header("Variables del Arma Raycast")]
+	public float force;
+	public Animator anim;
+	private RaycastHit hitInfo;
+	LineRenderer line;
+	void Start(){
+		if (spawnPoint == null)
+		{
+			spawnPoint = transform;
+		}
+		line = gameObject.AddComponent<LineRenderer> ();
+		line = gameObject.GetComponent<LineRenderer> ();
+		line.enabled = false;
+	}
+
+	public override void Sheathe()
+	{
+		anim.SetBool ("Load", false);
+	}
+
+	public override void UnSheathe()
+	{
+		anim.SetBool ("Load", true);
+	}
+
+	public override void Reload()
+	{
+		anim.SetTrigger("Reload");
+	}
+
+	public override void OnShoot()
+	{
+		Shoot();
+	}
+
+	public override void ExecuteShoot()
+	{
+		base.ExecuteShoot ();
+		anim.SetTrigger ("LightShot");
+		if (Physics.Raycast(spawnPoint.position, spawnPoint.forward, out hitInfo))
+		{
+			if (hitInfo.collider.gameObject.GetComponent<Rigidbody>())
+			{
+				hitInfo.collider.gameObject.GetComponent<Rigidbody> ().AddForce (spawnPoint.forward * force);
+			}
+			Debug.Log("Hitting " + hitInfo.collider.gameObject);
+			hitInfo.collider.gameObject.SendMessage ("ReceiveDmg", 1, SendMessageOptions.DontRequireReceiver);
+			Debug.DrawLine (spawnPoint.position, hitInfo.point, Color.red);
+			StartCoroutine (DrawLine ());
+		}
+		else
+		{
+			Debug.DrawRay (spawnPoint.position, spawnPoint.forward * 5, Color.green);
+		}
+	}
+
+	IEnumerator DrawLine()
+	{
+		line.positionCount = 2;
+		line.widthMultiplier = 0.01f;
+		line.SetPosition (0, spawnPoint.transform.position);
+		line.SetPosition (1, hitInfo.point);
+		line.startColor = Color.green;
+		line.endColor = Color.green;
+		line.enabled = true;
+		yield return new WaitForSeconds (0.02f);
+		line.enabled = false;
+	}
+}
---
+++ Enlightment/Assets/_root/Weapon_Management/Weapon_User.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPSBoys
+{
+	public class Weapon_User : MonoBehaviour {
+
+		public Weapon_Main[] myWeapons;
+		public int currentWeapon;
+
+		void OnEnable ()
+		{
+			//Manager_Static.inputManager.ShootDownHandler += UserShootDown;
+			Manager_Static.inputManager.ShootHandler += UserShoot;
+			Manager_Static.inputManager.ThrowHandler += UserThrow;
+			Manager_Static.inputManager.SwitchWeaponHandler += UserChangeWeapon;
+			//Debug.Log ("Subscribe to Input");
+		}
+
+		void OnDisable ()
+		{
+			//Manager_Static.inputManager.ShootDownHandler -= UserShootDown;
+			Manager_Static.inputManager.ShootHandler -= UserShoot;
+			Manager_Static.inputManager.ThrowHandler -= UserThrow;
+			Manager_Static.inputManager.SwitchWeaponHandler -= UserChangeWeapon;
+		}
+
+		void OnDestroy ()
+		{
+			//Manager_Static.inputManager.ShootDownHandler -= UserShootDown;
+			Manager_Static.inputManager.ShootHandler -= UserShoot;
+			Manager_Static.inputManager.ThrowHandler -= UserThrow;
+			Manager_Static.inputManager.SwitchWeaponHandler -= UserChangeWeapon;
+		}
+
+		// Update is called once per frame
+		void Update () {
+			// Mouse Testing Controls
+			if (Input.GetMouseButtonDown(0))
+			{
+				myWeapons [currentWeapon].OnShootDown ();
+			}
+			if (Input.GetMouseButton(0)) 
+			{
+				myWeapons [currentWeapon].OnShoot ();
+			}
+			if (Input.GetMouseButtonDown(1))
+			{
+				myWeapons [currentWeapon].OnThrowDown ();
+			}
+			if (Input.GetMouseButtonUp(0))
+			{
+				myWeapons [currentWeapon].OnShootUp ();
+			}
+			if (Input.GetKey(KeyCode.R))
+			{
+				myWeapons [currentWeapon].Reload ();
+			}
+			if (Input.GetKeyDown(KeyCode.Q))
+			{
+				PreviousWeapon ();
+			}
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				NextWeapon ();
+			}
+		}
+
+		public void UserShootDown(int _i)
+		{
+			myWeapons [currentWeapon].OnShootDown ();
+		}
+
+		public void UserShoot(int _i)
+		{
+			myWeapons [currentWeapon].OnShoot ();
+		}
+
+		public void UserShootUp()
+		{
+			myWeapons [currentWeapon].OnShootUp ();
+		}
+
+		public void UserReload()
+		{
+			myWeapons [currentWeapon].Reload ();
+		}
+
+		public void UserThrow(int _i)
+		{
+			myWeapons [currentWeapon].Throw ();
+		}
+
+		public void UserChangeWeapon(int i)
+		{
+			if (i == -1)
+				PreviousWeapon ();
+			if (i == 1)
+				NextWeapon ();
+		}
+
+		void NextWeapon()
+		{
+			myWeapons [currentWeapon].Sheathe ();
+			currentWeapon++;
+			if (currentWeapon >= myWeapons.Length)
+				currentWeapon = 0;
+			myWeapons [currentWeapon].UnSheathe ();
+		}
+
+		void PreviousWeapon()
+		{
+			myWeapons [currentWeapon].Sheathe ();
+			currentWeapon--;
+			if (currentWeapon < 0)
+				currentWeapon = myWeapons.Length - 1;
+			myWeapons [currentWeapon].UnSheathe ();
+		}
+	}
+}
