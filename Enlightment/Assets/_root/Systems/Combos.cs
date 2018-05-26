bbc145661f87c355106af64011cb20ab533d369b
Retus001 <retus001@gmail.com>
2018-05-26 06:56:02 -0500

    TAAI code now congratulates you

--- Enlightment/Assets/_root/Systems/Combos.cs
+++ Enlightment/Assets/_root/Systems/Combos.cs
@@ -2,26 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Combos : MonoBehaviour {
+namespace TAAI
+{
+	public class Combos : MonoBehaviour {
 
-	private KeyCombo falconPunch = new KeyCombo(new string[] {"down", "right","right"});
-	private KeyCombo falconKick = new KeyCombo(new string[] {"down", "right","Fire1"});
-	private KeyCombo easteregg = new KeyCombo(new string[] {"left", "right", "left", "right", "Right_Trigger", "Right_Trigger", "Control_Y"});
+		private KeyCombo falconPunch = new KeyCombo(new string[] {"down", "right","right"});
+		private KeyCombo falconKick = new KeyCombo(new string[] {"down", "right","Fire1"});
+		private KeyCombo easteregg = new KeyCombo(new string[] {"left", "right", "left", "right", "Right_Trigger", "Right_Trigger", "Control_Y"});
 
-	void Update () {
-		if (falconPunch.Check())
-		{
-			// do the falcon punch
-			Debug.Log("PUNCH"); 
-		}		
-		if (falconKick.Check())
-		{
-			// do the falcon punch
-			Debug.Log("KICK"); 
-		}
-		if (easteregg.Check())
-		{
-			Debug.Log ("EASTER EGG BOI");
+		void Update () {
+			if (falconPunch.Check())
+			{
+				// do the falcon punch
+				Debug.Log("PUNCH"); 
+			}		
+			if (falconKick.Check())
+			{
+				// do the falcon punch
+				Debug.Log("KICK"); 
+			}
+			if (easteregg.Check())
+			{
+				Manager_Static.scenManager.LoadSceneAdd (4);
+				Debug.Log ("EASTER EGG BOI");
+			}
 		}
 	}
 }
